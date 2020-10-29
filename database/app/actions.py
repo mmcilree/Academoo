8e387f18ff1027f474c395c114be055499697480
Scott Thomson <sthomson@localhost.localdomain>
2020-10-29 14:55:10 +0000

    almost finished action queries

--- database/app/actions.py
+++ database/app/actions.py
@@ -1,6 +1,7 @@
 from app import db
 from app.models import *
 import json
+import time
 
 def getCommunityIDs():
     ids = [community.id for community in Community.query.all()]
@@ -31,9 +32,10 @@ def createPost(json_file):
     author_dict = to_create["author"]
     author_id = author_dict["id"]
     author_host = author_dict["host"]
-    post_created = None
-    post_modified = None
-    post_community = None #?????????????
+    current_time = int(time.time())
+    post_created = current_time
+    post_modified = current_time
+    post_community = None # Still dont know where to find community in a post json doc help plz
 
     if User.query(User.id).filter_by(user_id = author_id).scalar() is not None:
         user = User(user_id = author_id, host = author_host)
@@ -50,9 +52,21 @@ def getPost(post_id):
     post_dict = {"id": post.id, "parent": post.parent_id, "children": [comment.id for comment in post.comments], "title": post.title, "content-type": post.content_type, "body": post.body, "author": {"id": post.admin.id, "host": post.admin.host}, "modified": post.modified, "created": post.created}
     return json.dumps(post_dict)
 
-def editPost(id, json_file):
+def editPost(post_id, json_file):
     to_update = json.loads(json_file)
+    update_title = to_update["title"]
+    update_content_type = to_update["content_type"]
+    update_body = to_update["body"]
+    update_modified = int(time.time())
 
+    post = Post.query.filter_by(id = post_id)
+    post.title = update_title
+    post.content_type = update_content_type
+    post.body = update_body
+    post.modified = update_modified
+    db.session.commit()
 
-def deletePost(id):
-    pass
+def deletePost(post_id):
+    post = Post.query.filter_by(id = post_id)
+    db.session.delete(post)
+    db.session.commit()
