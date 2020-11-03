ba1a57c39889fc5b6fc0762666ae7560e3346696
Rhona McCracken <ram31@st-andrews.ac.uk>
2020-11-03 22:14:52 +0000

    feat: added React Router to index.js and app.js
    Initial routing functionality added to navigate to "moosfeed" page

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import Button from 'react-bootstrap/Button';
+import React from "react";
+import Button from "react-bootstrap/Button";
+import { Route, Switch } from "react-router-dom";
+import PostsViewer from "./components/posts_viewer";
 
 class App extends React.Component {
   render() {
@@ -9,6 +11,11 @@ class App extends React.Component {
           <h1>Welcome to Academoo!</h1>
           <p>There's not much here yet...</p>
         </header>
+        <Switch>
+          <Route path="/moosfeed">
+            <PostsViewer />
+          </Route>
+        </Switch>
       </div>
     );
   }
--- client/src/index.js
+++ client/src/index.js
@@ -4,10 +4,13 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
 
   document.getElementById("root")
