7f82b09dfb24b5fd01381e53c29182f20873aa9e
moxis <mail@koxi.pw>
2020-11-03 21:04:28 +0000

    feat: user authentication and protected routes

--- server/config.py
+++ server/config.py
@@ -2,6 +2,11 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'spicy-secret'
+
     # Using SQLite as default database when one isn't defined
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    JWT_ACCESS_LIFESPAN = {'hours': 24}
+    JWT_REFRESH_LIFESPAN = {'days': 30}
