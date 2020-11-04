268b89c7cb45839cea8851e54ee162b186b2d609
moxis <mail@koxi.pw>
2020-11-04 13:33:58 +0000

    fix: nginx config file fix

--- server/boot.sh
+++ server/boot.sh
@@ -7,6 +7,7 @@ pip3 install -r requirements.txt
 flask db init
 flask db upgrade
 
+echo "Starting tmux session for backend"
 tmux kill-session -t CS3099-backend
 tmux new-session -d -s CS3099-backend "gunicorn -b :5000 backend:app"
 
