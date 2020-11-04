d0009c52bda8d7d73760f12980647cfb4acfae26
mjm42 <mjm42@st-andrews.ac.uk>
2020-11-04 16:16:20 +0000

    build: added way to start backend from npm

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,12 +1,30 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
-export default function Welcome() {
-    return (
-        <Card>
-            <div class="container">
-                <h1>Welcome to Academoo!</h1>
-                <p>There's not much here yet...</p>
-            </div>
-        </Card>
-    );
+import axios from 'axios';
+
+class Welcome extends React.Component {
+    state = {
+        message: ""
+    }
+    componentDidMount() {
+        axios.get('https://nnv2.host.cs.st-andrews.ac.uk/fed/')
+            .then(res => {
+                const message = res.data;
+                this.setState({ message: message });
+            })
+        console.log(this.state.message);
+    }
+
+    render() {
+        return (
+            <Card>
+                <div className="container">
+                    <h1>Welcome to Academoo!</h1>
+                    <p>{this.state.message}</p>
+                </div>
+            </Card>
+        );
+    }
 }
+
+export default Welcome;
