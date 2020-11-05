71145ec2b439ba163148bf2f1a797d24aafe6924
Rhona McCracken <ram31@st-andrews.ac.uk>
2020-11-05 22:59:55 +0000

    fix: fonts applied to whole site and scss replaces css

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./custom.css";
 import "./index.css";
 import App from "./App";
+import "./App.scss";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
