fadff983b693c10566ffbd3e5100b6b48fcc1b52
Nina B <ninab@ninas-mbp.home>
2020-11-08 20:50:48 +0000

    feat: added login files

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
+import { Button, FormGroup, FormControl, Form } from "react-bootstrap";
 
-class Login extends Component {
+class Login extends React.Component {
   render() {
-    return <h2>Login</h2>;
+    return (
+        <div className="mx-auto">
+        <form>
+          <FormGroup controlId="email" bsSize="large">
+            <Form.Label>Email</Form.Label>
+            <FormControl
+              autoFocus
+              type="email"
+             // value={email}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <Form.Label>Password</Form.Label>
+            <FormControl
+              //value={password}
+              type="password"
+            />
+          </FormGroup>
+          <Button variant="primary" block bsSize="large"  type="submit">
+            Login
+          </Button>
+        </form>
+        <Button variant="secondary" block bsSize="large" type="submit">
+            No login? Sign up here!
+          </Button>
+      </div>
+    );
   }
 }
 
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,8 +1,57 @@
-import React, { Component } from "react";
+import React from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./SignUp.css"
 
-class SignUp extends Component {
+class SignUp extends React.Component {
   render() {
-    return <h2>Sign Up</h2>;
+    return (
+        <div className="SignUp">
+            <p>Sign up for your Academoo account here. </p>
+        <form>
+        <FormGroup controlId="firstName" bsSize="large">
+            <ControlLabel>First Name</ControlLabel>
+            <FormControl
+              autoFocus
+              type="text"
+              //value={email}
+            />
+          </FormGroup>
+          <FormGroup controlId="secondName" bsSize="large">
+            <ControlLabel>Second Name</ControlLabel>
+            <FormControl
+              autoFocus
+              type="text"
+              //value={email}
+            />
+          </FormGroup>
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              //value={email}
+            />
+          </FormGroup>
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              //value={password}
+              type="password"
+            />
+          </FormGroup>
+          <FormGroup controlId="confirmPassword" bsSize="large">
+            <ControlLabel>Confirm Password</ControlLabel>
+            <FormControl
+              //value={password}
+              type="password"
+            />
+          </FormGroup>
+          <Button variant="primary" block bsSize="large"   type="submit">
+            Register
+          </Button>
+        </form>
+      </div>
+    );
   }
 }
 
