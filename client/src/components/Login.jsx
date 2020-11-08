fe72f5ae4d520cbec5006e416e9a3bce81d1385e
Nina B <ninab@ninas-mbp.home>
2020-11-08 21:09:35 +0000

    feat: added presentation to pages

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,34 +1,38 @@
 import React from "react";
-import { Button, FormGroup, FormControl, Form } from "react-bootstrap";
+import { Button, FormGroup, FormControl, Form, Card } from "react-bootstrap";
 
 class Login extends React.Component {
   render() {
     return (
-        <div className="mx-auto">
-        <form>
-          <FormGroup controlId="email" bsSize="large">
-            <Form.Label>Email</Form.Label>
-            <FormControl
-              autoFocus
-              type="email"
-             // value={email}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <Form.Label>Password</Form.Label>
-            <FormControl
-              //value={password}
-              type="password"
-            />
-          </FormGroup>
-          <Button variant="primary" block bsSize="large"  type="submit">
-            Login
+      <Card className="mt-4">
+        <Card.Body>
+          <div className="mx-auto">
+            <form>
+              <FormGroup controlId="email" bsSize="large">
+                <Form.Label>Email</Form.Label>
+                <FormControl
+                  autoFocus
+                  type="email"
+                // value={email}
+                />
+              </FormGroup>
+              <FormGroup controlId="password" bsSize="large">
+                <Form.Label>Password</Form.Label>
+                <FormControl
+                  //value={password}
+                  type="password"
+                />
+              </FormGroup>
+              <Button variant="primary" block bsSize="large" type="submit">
+                Login
           </Button>
-        </form>
-        <Button variant="secondary" block bsSize="large" type="submit">
-            No login? Sign up here!
+            </form>
+            <Button variant="secondary" block bsSize="large" type="submit">
+              No login? Sign up here!
           </Button>
-      </div>
+          </div>
+        </Card.Body>
+      </Card>
     );
   }
 }
--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,56 +1,59 @@
 import React from "react";
-import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
-import "./SignUp.css"
+import { Button, FormGroup, FormControl, Form, Card} from "react-bootstrap";
 
 class SignUp extends React.Component {
   render() {
     return (
-        <div className="SignUp">
+      <Card className="mt-4">
+        <Card.Body>
+          <div className="SignUp">
             <p>Sign up for your Academoo account here. </p>
-        <form>
-        <FormGroup controlId="firstName" bsSize="large">
-            <ControlLabel>First Name</ControlLabel>
-            <FormControl
-              autoFocus
-              type="text"
-              //value={email}
-            />
-          </FormGroup>
-          <FormGroup controlId="secondName" bsSize="large">
-            <ControlLabel>Second Name</ControlLabel>
-            <FormControl
-              autoFocus
-              type="text"
-              //value={email}
-            />
-          </FormGroup>
-          <FormGroup controlId="email" bsSize="large">
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-              //value={email}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              //value={password}
-              type="password"
-            />
-          </FormGroup>
-          <FormGroup controlId="confirmPassword" bsSize="large">
-            <ControlLabel>Confirm Password</ControlLabel>
-            <FormControl
-              //value={password}
-              type="password"
-            />
-          </FormGroup>
-          <Button variant="primary" block bsSize="large"   type="submit">
-            Register
+            <form>
+              <FormGroup controlId="firstName" bsSize="large">
+                <Form.Label>First Name</Form.Label>
+                <FormControl
+                  autoFocus
+                  type="text"
+                //value={email}
+                />
+              </FormGroup>
+              <FormGroup controlId="secondName" bsSize="large">
+                <Form.Label>Second Name</Form.Label>
+                <FormControl
+                  autoFocus
+                  type="text"
+                //value={email}
+                />
+              </FormGroup>
+              <FormGroup controlId="email" bsSize="large">
+                <Form.Label>Email</Form.Label>
+                <FormControl
+                  autoFocus
+                  type="email"
+                //value={email}
+                />
+              </FormGroup>
+              <FormGroup controlId="password" bsSize="large">
+                <Form.Label>Password</Form.Label>
+                <FormControl
+                  //value={password}
+                  type="password"
+                />
+              </FormGroup>
+              <FormGroup controlId="confirmPassword" bsSize="large">
+                <Form.Label>Confirm Password</Form.Label>
+                <FormControl
+                  //value={password}
+                  type="password"
+                />
+              </FormGroup>
+              <Button variant="primary" block bsSize="large" type="submit">
+                Register
           </Button>
-        </form>
-      </div>
+            </form>
+          </div>
+        </Card.Body>
+      </Card>
     );
   }
 }
