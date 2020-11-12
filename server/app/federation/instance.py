1233bf2a36326cb640c02d7c0209a901e2a8f644
moxis <mail@koxi.pw>
2020-11-12 03:37:20 +0000

    feat: federation backend getting posts from other instances

--- server/app/federation/instance.py
+++ server/app/federation/instance.py
@@ -5,12 +5,11 @@ class Instance(object):
     def __init__(self, url):
         self.url = url
     
-
     def get_timestamps(self, community):
-        ret = requests.get(urljoin(self.url, f"/fed/communities/{community}/timestamps"))
-        print(ret.text)
+        ret = requests.get(urljoin(self.url, f"/api/communities/{community}/timestamps"))
         return ret.json()
 
     # If the timestamp is different, then the cache is invalidated
-    def get_posts():
-        pass
+    def get_posts(self, community):
+        ret = requests.get(urljoin(self.url, f"/api/posts?community={community}"))
+        return ret.json()
--- server/app/federation/manager.py
+++ server/app/federation/manager.py
@@ -5,12 +5,21 @@ from app.federation.instance import Instance
 class Manager(object):
     def __init__(self):
         # host name : <Instance Objects>
-        self.instances = {
-            "test": Instance("https://nnv2.host.cs.st-andrews.ac.uk/")
-        }
+        self.instances = {}
     
     def _get_latest_timestamp(self, host, community):
-        return self.instances[host].get_timestamps(community)
+        return max([x["timestamp"] for x in self.instances[host].get_timestamps(community)] + [0])
 
-    def get_posts(host, community):
-        pass
+    @functools.lru_cache()
+    def _get_posts(self, host, community, _timestamp): # timestamp purely for caching purposes
+        return self.instances[host].get_posts(community)
+
+    def get_posts(self, host, community):
+         timestamp = self._get_latest_timestamp(host, community)
+         return self._get_posts(host, community, timestamp)
+        
+    def add_instance(self, host, url):
+        if host in self.instances: return False
+
+        self.instances[host] = Instance(url)
+        return True
