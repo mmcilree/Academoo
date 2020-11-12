2ca42e5e3355f9115a75abf5f788eee2c7bd1bd0
mjm42 <mjm42@st-andrews.ac.uk>
2020-11-12 09:10:04 +0000

    feat: Added user data to profile page.

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -1,8 +1,34 @@
 import React, { Component } from "react";
 import { Card, Media } from "react-bootstrap";
 import defaultProfile from "../images/default_profile.png";
+import { authFetch } from '../auth';
 
 class UserProfile extends Component {
+  constructor(props) {
+      super(props);
+      this.state = { 
+        user_id: "",
+        email: "", 
+        host: "", 
+        isLoading: true};
+  } 
+
+  fetchUserDetails() {
+    authFetch("/api/get-user").then(response => response.json())
+      .then(data =>
+        this.setState({
+          user_id: data.id,
+          email: data.email,
+          host: data.host,
+          isLoading: false
+        })
+      )
+  }
+
+  componentDidMount() {
+    this.fetchUserDetails();
+  }
+
   render() {
     return (
       <Card className="mt-4">
@@ -11,14 +37,15 @@ class UserProfile extends Component {
             <img
               width={150}
               height={150}
-              className="mr-3 rounded-circle border border-secondary"
+              className="mr-3 rounded-circle border border-primary"
               src={defaultProfile}
               alt="Profile image placeholder"
             />
+            {!this.state.isLoading ? (
             <Media.Body className="mr-3">
-              <h3>Profile page</h3>
-              <p>This is the user's profile page! </p>
-            </Media.Body>
+              <h3>Username: {this.state.user_id}</h3>
+              <h4 className="text-muted"> Email: {this.state.email} </h4>
+            </Media.Body> ) : <h3>Loading Profile...</h3> }
           </Media>
         </Card.Body>
       </Card>
