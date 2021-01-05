95f4af81c6310682bedd863442c76b83dc6f3229
mjm42 <mjm42@st-andrews.ac.uk>
2021-01-05 10:51:03 +0000

    feat: Explore lists now fetches communities from each available instance.

--- client/src/components/CommunityList.jsx
+++ client/src/components/CommunityList.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 import Accordion from "react-bootstrap/Accordion";
-import AccordionContext from "react-bootstrap/AccordionContext";
 import Card from "react-bootstrap/Card";
-import { useAccordionToggle } from "react-bootstrap/AccordionToggle";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ListGroup from "react-bootstrap/ListGroup";
 import { useContext } from "react";
@@ -16,22 +14,46 @@ import {
 class CommunityList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            communities: [],
+            isLoading: true,
+        }
     }
+
+    componentDidMount() {
+        this.fetchCommunities();
+    }
+
+    fetchCommunities() {
+        fetch('/api/communities' + (this.props.instance !== "local" ? "?external=" + this.props.instance : ""))
+            .then(response => response.json())
+            .then(data =>
+                this.setState({ 
+                    communities: data,
+                    isLoading: false,
+                })
+            )
+    }
+
     render() {
+        const {communities, isLoading} = this.state;
+        console.log(communities);
         return (
             <Accordion defaultActiveKey="0">
                 <Card className="mt-4">
                     <Accordion.Toggle as={Card.Header} eventKey="0" className="pt-4">
-                        <h5>Hosted {this.props.instance === "local" ? "locally" : "on '" + this.props.instance + "':"}
+                        <h5>Hosted {this.props.instance === "local" ? "locally:" : "on '" + this.props.instance + "':"}
                         </h5>
                     </Accordion.Toggle>
 
                     <Accordion.Collapse eventKey="0">
                         <Card.Body className="px-0 py-1">
                             <ListGroup variant="flush">
-                                <ListGroup.Item>Community 1</ListGroup.Item>
-                                <ListGroup.Item>Community 2</ListGroup.Item>
-                                <ListGroup.Item>Community 3</ListGroup.Item>
+                                {!isLoading ? 
+                                    communities.map((community) =>
+                                        community != "" && <ListGroup.Item>{community}</ListGroup.Item>)
+                                    : <ListGroup.Item>Loading Communities...</ListGroup.Item>
+                                }
                             </ListGroup>
                         </Card.Body>
                     </Accordion.Collapse>
