38359caecd8ea677e35b948b9bab9529ec376e94
mjm42 <mjm42@st-andrews.ac.uk>
2021-01-07 19:56:49 +0000

    test: Finished logged in routing tests.

--- client/src/tests/LoggedInRouting.test.js
+++ client/src/tests/LoggedInRouting.test.js
@@ -5,7 +5,7 @@ configure({ adapter: new Adapter() });
 import { shallow, mount } from "enzyme";
 import App from "../App";
 import Welcome from "../components/Welcome";
-import PostsViewer from "../components/PostsViewer";
+import CommunityFeed from "../components/CommunityFeed";
 import CommentsViewer from "../components/CommentsViewer";
 import PostCreator from "../components/PostCreator";
 import UserSettings from "../components/UserSettings";
@@ -13,104 +13,102 @@ import UserProfile from "../components/UserProfile";
 import Login from "../components/Login";
 import SignUp from "../components/SignUp";
 import { MemoryRouter as Router, Route } from "react-router-dom";
-import { JustifyLeft } from "react-bootstrap-icons";
-
 
 // const App = require('../App').default;
 const auth = require('../auth');
 
+// Mock the necessary API calls for authentication
 jest.mock('../auth', () => ({
-  useAuth: () => [true],
-  }));
+    useAuth: () => [true],
+    authFetch: () =>
+        Promise.resolve({
+            json: () => Promise.resolve(
+                { 
+                    id: "academoo",
+                    email: "academoo@academoo.com",
+                    host: "academoo"
+                }),
+        })
+    }));
 
 it("Routes to default page", () => {
-  auth.useAuth.mockReturnValue([true]);
-  const wrapper = mount(
-    <Router initialEntries={["/"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(Welcome)).toHaveLength(1);
 });
 
 it("Routes to login page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/login"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/login"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(Welcome)).toHaveLength(1);
 });
 
 it("Routes to sign-up page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/sign-up"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(SignUp)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/sign-up"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(Welcome)).toHaveLength(1);
 });
 
 
 it("Routes to welcome page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/home"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/home"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(Welcome)).toHaveLength(1);
 });
 
 it("Routes to Moosfeed page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/moosfeed"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/moosfeed"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(CommunityFeed)).toHaveLength(1);
 });
 
 it("Routes to Create-Post page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/create-post"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/create-post"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(PostCreator)).toHaveLength(1);
 });
 
 it("Routes to Comments page for parent post id", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/moosfeed/comments/post1"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/moosfeed/comments/post1"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(CommentsViewer)).toHaveLength(1);
 });
 
 it("Routes to User-Profile page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/user-profile"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/user-profile"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(UserProfile)).toHaveLength(1);
 });
 
 it("Routes to User-Settings page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/user-settings"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
-});
-
-it("Routes to User-Settings page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/user-settings"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
+    const wrapper = mount(
+        <Router initialEntries={["/user-settings"]}>
+            <App />
+        </Router>
+    );
+    expect(wrapper.find(UserSettings)).toHaveLength(1);
 });
--- client/src/tests/LoggedOutRouting.test.js
+++ client/src/tests/LoggedOutRouting.test.js
@@ -13,19 +13,8 @@ import UserProfile from "../components/UserProfile";
 import Login from "../components/Login";
 import SignUp from "../components/SignUp";
 import { MemoryRouter as Router, Route } from "react-router-dom";
-import { JustifyLeft } from "react-bootstrap-icons";
-
-
-
-// const App = require('../App').default;
-const auth = require('../auth');
-
-jest.mock('../auth', () => ({
-  useAuth: () => [true],
-  }));
 
 it("Routes to default page", () => {
-  auth.useAuth.mockReturnValue([true]);
   const wrapper = mount(
     <Router initialEntries={["/"]}>
       <App />
@@ -92,15 +81,6 @@ it("Routes to Comments page for parent post id", () => {
 it("Routes to User-Profile page", () => {
   const wrapper = mount(
     <Router initialEntries={["/user-profile"]}>
-      <App />
-    </Router>
-  );
-  expect(wrapper.find(Login)).toHaveLength(1);
-});
-
-it("Routes to User-Settings page", () => {
-  const wrapper = mount(
-    <Router initialEntries={["/user-settings"]}>
       <App />
     </Router>
   );
