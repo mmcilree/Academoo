3c7ed8f6325853e277afff21aa82afca06577d62
RAMcCracken <rhona.mccracken@btinternet.com>
2021-01-21 16:14:13 +0000

    feat:add endpoints for getting all users

--- server/app/supergroup_protocol/routes.py
+++ server/app/supergroup_protocol/routes.py
@@ -3,6 +3,25 @@ from app.supergroup_protocol import bp
 from flask import jsonify, request, Response
 from app.models import User
 
+# User
+@bp.route("/users", methods=["GET"])
+def get_all_users():
+    external = request.args.get("external")
+
+    if not external:
+        return jsonify(actions.getUserIDs())
+    else:
+        return jsonify(federation.get_users(external))
+
+@bp.route("/users/<id>", methods=["GET"])
+def get_user_by_id(id):
+    external = request.args.get("external")
+
+    if not external:
+        return jsonify(actions.getUser(id))
+    else:
+        return jsonify(federation.get_users(external, id=id))        
+
 # Community
 @bp.route("/communities", methods=["GET"])
 def get_all_communities():
