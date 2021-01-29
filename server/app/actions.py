7674303b06cca5c85837b8a23e5ee01204d36ab4
RAMcCracken <rhona.mccracken@btinternet.com>
2021-01-29 15:48:23 +0000

    fix: merge request conflicts

--- server/app/actions.py
+++ server/app/actions.py
@@ -37,6 +37,11 @@ def createUser(username, email, password):
     
     return False
 
+def updateBio(user_id, bio):
+    user = User.query.filter_by(user_id = user_id).first()
+    user.bio = bio
+    db.session.commit()
+
 def getUserIDs():
     ids = [user.user_id for user in User.query.all()]
     return ids
