27bc8da06a5e8ee55a1ff8b60404328a4528e870
moxis <mail@koxi.pw>
2021-02-02 14:43:14 +0000

    feat: pseudo account deployment

--- boot.sh
+++ boot.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-cp ./deployment/nginx.conf /host/nnv2/nginx.d/default/CS3099.conf
-nginx -c /host/nnv2/nginx.conf -s reload
+cp ./deployment/nginx.conf /host/cs3099user-a1/nginx.d/default/CS3099.conf
+nginx -c /host/cs3099user-a1/nginx.conf -s reload
 
 cd server && chmod +x boot.sh && sh boot.sh && cd ..
 cd client && chmod +x boot.sh && sh boot.sh && cd ..
