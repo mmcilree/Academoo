a2eb8ab9a688c4988a304a102892f760ad247ed1
Nina B <ninab@ninas-mbp.home>
2021-02-09 16:06:17 +0000

    feat: improved welcome page

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -4,20 +4,34 @@ import Button from "react-bootstrap/Button";
 import { Link } from "react-router-dom";
 import { authFetch } from "../auth";
 
+import {
+  BadgeTm,
+} from "react-bootstrap-icons";
+
 class Welcome extends React.Component {
   render() {
     return (
       <Card className="mt-4">
         <Card.Body>
           <h1>Hello,</h1>
-          <h1>Welcome to Academoo!</h1>
-          <p>We are part of a new federated social media platform for universities... </p>
-          <p>There is lots of things you can do on our website, if you need any help operating our social media site, visit our help page!</p>
-          <Link to="/moosfeed" className="btn btn-secondary">
-            Go to Moosfeed
+          <h1>  Welcome to Academoo!</h1>
+          <p>We are part of a new federated social media platform for universities. We connect with similar sites to allow communciation between sites and to allow people from all over to connect and discuss things they enjoy! </p>
+          <p>There is lots of things you can do on Academoo: 
+            <ul>
+              <li>Create your own community</li>
+              <li>Post on communities on our server, and others</li>
+              <li>Upvote and downvote members posts</li>
+              <li>Personalise your profile</li>
+              <li>Much, much more...</li>
+              </ul>
+              If you need any help operating our social media site, visit our <Link to="/help">help page</Link> for guidance.</p>
+          <p>To get started, scroll through our list of communities and follow ones you are interested in!</p>
+          <Link to="/explore" className="btn btn-secondary">
+            Explore Commoonities
           </Link>
         </Card.Body>
       </Card>
+      
     );
   }
 }
