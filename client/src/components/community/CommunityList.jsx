478e7697ebca6af809f1befb0991d9eec4238848
RAMcCracken <rhona.mccracken@btinternet.com>
2021-03-15 19:45:40 +0000

    style: moved create community button

--- client/src/components/community/CommunityList.jsx
+++ client/src/components/community/CommunityList.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import {Accordion, Alert } from "react-bootstrap";
+import { Accordion, Alert, Button } from "react-bootstrap";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 import ListGroup from "react-bootstrap/ListGroup";
 import { Link } from "react-router-dom";
 import CommunitySubscribeButton from "./CommunitySubscribeButton";
+import { PlusCircle } from "react-bootstrap-icons"
 
 class CommunityList extends React.Component {
     constructor(props) {
@@ -21,44 +22,56 @@ class CommunityList extends React.Component {
     }
 
     fetchCommunities() {
-        fetch('/api/communities' + (this.props.instance !== "local" ? "?external=" + this.props.instance : ""), 
-        {
-            headers: {
-                'Client-Host': window.location.protocol + "//" + window.location.hostname
-            }
-        })
+        fetch('/api/communities' + (this.props.instance !== "local" ? "?external=" + this.props.instance : ""),
+            {
+                headers: {
+                    'Client-Host': window.location.protocol + "//" + window.location.hostname
+                }
+            })
             .then(response => {
-                if(!response.ok) {
+                if (!response.ok) {
                     throw new Error();
                 }
                 return response.json()
             }
             )
             .then(data => {
-                
+
                 this.setState({
                     communities: [...this.state.communities, ...data],
                     isLoading: false
                 })
-                
+
             }
             ).catch((err) => {
-                this.setState({errors: [...this.state.errors, err.message], isLoading: false});
-                });
+                this.setState({ errors: [...this.state.errors, err.message], isLoading: false });
+            });
 
     }
 
     render() {
         const { communities, isLoading, errors } = this.state;
-        
+
         return (
             <Accordion defaultActiveKey="0">
                 <Card className="mt-4">
-                    <Accordion.Toggle as={Card.Header} eventKey="0" className="pt-4">
-                        <h5>Hosted {this.props.instance === "local" ? "locally:" : "on '" + this.props.instance + "':"}
-                        </h5>
-                    </Accordion.Toggle>
 
+                    {this.props.instance === "local" ?
+                        <Accordion.Toggle as={Card.Header} eventKey="0" className="d-flex justify-content-between pt-4">
+                            <h5>Hosted locally</h5>
+                            <Link to="/create-community">
+                                <Button variant="secondary">
+                                    <PlusCircle className="mb-1" />
+                                    <span> New Commoonity</span>
+                                </Button>
+                            </Link>
+                        </Accordion.Toggle>
+                        :
+                        <Accordion.Toggle as={Card.Header} eventKey="0" className="pt-4">
+                            <h5>Hosted {this.props.instance === "local" ? "locally:" : "on '" + this.props.instance + "':"}
+                            </h5>
+                        </Accordion.Toggle>
+                    }
                     <Accordion.Collapse eventKey="0">
                         <Card.Body className="px-0 py-1">
                             <ListGroup variant="flush">
@@ -67,7 +80,7 @@ class CommunityList extends React.Component {
                                     <ListGroup.Item key={error}>
                                         <Alert variant='warning' key={error}>{"Error: " + error}</Alert>
                                     </ListGroup.Item>
-                                    
+
                                 ))}
 
                                 {!isLoading ?
