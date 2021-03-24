2a575bf6b69487011200b63fd3440c5f95ca7a84
mjm42 <mjm42@st-andrews.ac.uk>
2021-03-24 18:23:37 +0000

    fix: change back to authfetch

--- client/src/components/user/SubscribedFeed.jsx
+++ client/src/components/user/SubscribedFeed.jsx
@@ -43,7 +43,7 @@ class SubscribedFeed extends Component {
     }
 
     async appendPostsFromCommunity(community, i) {
-        await fetch('/api/posts?community=' + community + '&includeSubChildrenPosts=false',
+        await authFetch('/api/posts?community=' + community + '&includeSubChildrenPosts=false',
             {
                 headers: {
                     'User-ID': this.state.user_id,
