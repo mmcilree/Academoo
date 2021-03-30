5d3091aa12c71ab95aed395efcfbf3eb8ea2e512
mjm42 <mjm42@st-andrews.ac.uk>
2021-03-30 17:42:33 +0100

    feat: Added backend socket support for rooms feature.

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate
 from flask_cors import CORS
 from flask_praetorian import Praetorian, current_user
 from app.federation.manager import Manager
+from flask_socketio import SocketIO
 
 from config import Config
 
@@ -17,6 +18,9 @@ db = SQLAlchemy()
 migrate = Migrate(compare_type=True)
 guard = Praetorian()
 instance_manager = Manager(app.config["HOST"])
+socketio = SocketIO()
+
+
 
 from app.models import User
 
@@ -28,6 +32,8 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     guard.init_app(app, User)
+    socketio.init_app(app, cors_allowed_origins="*")
+    socketio.run(app)
 
     url_prefix = "/" if os.environ.get("FLASK_ENV") == "production" else "/api"
 
