68a94cc5d82bec8922ec2232da74c8a66950e3cf
moxis <mail@koxi.pw>
2021-04-15 00:51:14 +0100

    fix: removed those node options

--- client/boot.sh
+++ client/boot.sh
@@ -5,6 +5,6 @@ echo "Starting tmux session for frontend"
 kill $(lsof -t -i:3000)
 kill $(lsof -t -i:21767)
 tmux kill-session -t CS3099-frontend
-tmux new-session -d -s CS3099-frontend "export NODE_OPTIONS='--max-old-space-size=8192' && npm install && npm run build && node_modules/serve/bin/serve.js -s build -l 21767"
+tmux new-session -d -s CS3099-frontend "npm install && npm run build && node_modules/serve/bin/serve.js -s build -l 21767"
 # tmux new-session -d -s CS3099-frontend "npm install && npm run build && npm start"
 
